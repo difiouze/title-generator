ee46e319d24c2cd47f75112a7fe2fc85df946402
laurentravix <laurentravix@gmail.com>
2020-09-30 19:27:39 +0200

    number + day + month

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
+import numbers from './Utils/Numbers';
 import days from './Utils/Days';
+import months from './Utils/Months';
 import './App.css';
 
 function App() {
-  const [dayStorage, setDayStorage] = useState(["Click the button to pick a day"]);
- 
+  const [numberStorage, setNumberStorage] = useState("Number");
+  const [dayStorage, setDayStorage] = useState("Day");
+  const [monthStorage, setMonthStorage] = useState("Month");
+
   const randomDay = () => {
-     const day = days[Math.floor(Math.random() * days.length)];
-     setDayStorage(day);
+    const number = numbers[Math.floor(Math.random() * numbers.length)];
+    const day = days[Math.floor(Math.random() * days.length)];
+    const month = months[Math.floor(Math.random() * months.length)];
+    setDayStorage(day);
+    setNumberStorage(number);
+    setMonthStorage(month);
   }
 
   const reset = () => {
-    setDayStorage(["Click the button to pick a day"]);
+    setNumberStorage("Number");
+    setDayStorage("Day");
+    setMonthStorage("Month");
   }
 
   return (
     <div className="App">
-      <h1>{dayStorage}</h1>
+      <h1> {numberStorage}  {dayStorage}  {monthStorage}</h1>
       <button onClick={randomDay}>Click me !</button>
       <button onClick={reset}>Back to title</button>
     </div>
